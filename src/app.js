19567544e94a78175a94cf60d52f749d5ff7c520
Florian Mauracher <flo@mauracher.eu>
2018-07-24 03:54:37 +0200

    Add basic Cesium Viewer init

--- src/app.js
+++ src/app.js
@@ -9,6 +9,29 @@ if (process.env.NODE_ENV === "dev") {
 require("../lib/Cesium/Widgets/widgets.css")
 require('./css/main.css');
 
+var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+var isLocalOnly = true;
+
 var Cesium = window.Cesium
 
-var viewer = new Cesium.Viewer("cesiumContainer")
+var viewer = new Cesium.Viewer("cesiumContainer", {
+  imageryProvider: isLocalOnly ?
+    new Cesium.createTileMapServiceImageryProvider({
+      url : Cesium.buildModuleUrl('Assets/Textures/NaturalEarthII')
+    }) :
+    new Cesium.ArcGisMapServerImageryProvider({
+      url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+    }),
+  baseLayerPicker: !isLocalOnly,
+  animation: !isIOS,
+  geocoder: false,
+  timeline: !isIOS,
+  vrButton: true,
+  fullscreenButton: !isIOS,
+  fullscreenElement: document.body,
+  navigationInstructionsInitiallyVisible: false,
+});
+
+viewer.scene.globe.enableLighting = false;
+viewer.scene.fog.enabled = false;
+viewer.scene.debugShowFramesPerSecond = true;
