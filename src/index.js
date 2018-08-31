9fe8efde70727b1742de598a8ad6a4da20ddf80c
Florian Mauracher <flo@mauracher.eu>
2018-08-31 23:05:41 +0200

    Create module to handle satellite entity creation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Cesium from "cesium/Cesium";
-import { SatelliteOrbit } from "./modules/orbit";
+import { SatelliteEntity } from "./modules/satellite";
 
 import "cesium/Widgets/widgets.css";
 import "./css/main.css";
@@ -29,51 +29,8 @@ viewer.scene.globe.enableLighting = false;
 viewer.scene.fog.enabled = false;
 viewer.scene.debugShowFramesPerSecond = true;
 
-// Satellite Position
-const satelliteTLE = "0 First-MOVE\n1 39439U 13066Z   18203.92296999 +.00000436 +00000-0 +59983-4 0  9994\n2 39439 097.5919 229.8528 0066721 040.9363 319.6832 14.81533022250876";
-const satellite = new SatelliteOrbit(satelliteTLE);
-function computeSatellitePosition(timestamp) {
-  const position = satellite.computeOrbitTrack(Cesium.JulianDate.toDate(timestamp), 1);
-  if (position.length < 3) {
-    return Cesium.Cartesian3.fromRadians(0, 0, 0);
-  }
-  return Cesium.Cartesian3.fromRadians(position[0], position[1], position[2]);
-}
-
-// Satellite Model
-const satelliteProperties = {
-  name: "MOVE-II",
-  size: 1000.0,
-  viewFrom: new Cesium.Cartesian3(0, -1200000, 1150000),
-};
-
-const satelliteLabel = new Cesium.LabelGraphics({
-  text: satelliteProperties.name,
-  horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
-  pixelOffset: new Cesium.Cartesian2(10, 0),
-  distanceDisplayCondition: new Cesium.DistanceDisplayCondition(satelliteProperties.size * 5, 5.0e7),
-  pixelOffsetScaleByDistance: new Cesium.NearFarScalar(satelliteProperties.size, 50, 2.0e5, 1),
-});
-
-const satellitePoint = new Cesium.PointGraphics({
-  pixelSize: 10,
-  color: Cesium.Color.WHITE,
-});
-
-const satelliteDummy = new Cesium.BoxGraphics({
-  dimensions: new Cesium.Cartesian3(satelliteProperties.size, satelliteProperties.size, satelliteProperties.size),
-  material: Cesium.Color.WHITE,
-});
-
-const satelliteEntity = new Cesium.Entity({
-  name: satelliteProperties.name,
-  point: satellitePoint,
-  box: satelliteDummy,
-  size: satelliteProperties.size,
-  label: satelliteLabel,
-  viewFrom: satelliteProperties.viewFrom,
-  position: new Cesium.CallbackProperty(computeSatellitePosition, false),
-});
-
-viewer.entities.add(satelliteEntity);
-viewer.trackedEntity = satelliteEntity;
+// Create satellite
+const dummyTLE = "0 First-MOVE\n1 39439U 13066Z   18203.92296999 +.00000436 +00000-0 +59983-4 0  9994\n2 39439 097.5919 229.8528 0066721 040.9363 319.6832 14.81533022250876";
+const satellite = new SatelliteEntity(dummyTLE);
+satellite.add(viewer);
+satellite.track(viewer);
---
+++ src/modules/satellite.js
@@ -0,0 +1,61 @@
+import Cesium from "cesium/Cesium";
+import { SatelliteOrbit } from "./orbit";
+
+export class SatelliteEntity {
+  constructor(tle) {
+    this.name = tle.split("\n")[0].split(" ")[1];
+    this.orbit = new SatelliteOrbit(tle);
+    this.size = 1000;
+    this.entity = this.createSatelliteEntity();
+  }
+
+  add(viewer) {
+    viewer.entities.add(this.entity);
+  }
+
+  remove(viewer) {
+    viewer.entities.remove(this.entity);
+  }
+
+  track(viewer) {
+    viewer.trackedEntity = this.entity;
+  }
+
+  createSatelliteEntity() {
+    const satelliteLabel = new Cesium.LabelGraphics({
+      text: this.name,
+      horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+      pixelOffset: new Cesium.Cartesian2(10, 0),
+      distanceDisplayCondition: new Cesium.DistanceDisplayCondition(this.size * 5, 5.0e7),
+      pixelOffsetScaleByDistance: new Cesium.NearFarScalar(this.size, 50, 2.0e5, 1),
+    });
+
+    const satellitePoint = new Cesium.PointGraphics({
+      pixelSize: 10,
+      color: Cesium.Color.WHITE,
+    });
+
+    const satelliteBox = new Cesium.BoxGraphics({
+      dimensions: new Cesium.Cartesian3(this.size, this.size, this.size),
+      material: Cesium.Color.WHITE,
+    });
+
+    const satelliteEntity = new Cesium.Entity({
+      name: this.name,
+      point: satellitePoint,
+      box: satelliteBox,
+      size: this.size,
+      label: satelliteLabel,
+      viewFrom: new Cesium.Cartesian3(0, -1200000, 1150000),
+      position: new Cesium.CallbackProperty((timestamp) => {
+        const position = this.orbit.computeOrbitTrack(Cesium.JulianDate.toDate(timestamp), 1);
+        if (position.length < 3) {
+          return Cesium.Cartesian3.fromRadians(0, 0, 0);
+        }
+        return Cesium.Cartesian3.fromRadians(position[0], position[1], position[2]);
+      }, false),
+    });
+
+    return satelliteEntity;
+  }
+}
