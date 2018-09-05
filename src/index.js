bf28fe6acd41447bf0c722c90c45514c8c7b31eb
Florian Mauracher <flo@mauracher.eu>
2018-09-05 20:13:22 +0200

    Fix eslint style warnings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Cesium from "Cesium";
-import Vue from "vue"
+import Vue from "vue";
 import { SatelliteManager } from "./modules/SatelliteManager";
 
 import "cesium/Widgets/widgets.css";
@@ -30,8 +30,8 @@ const sats = new SatelliteManager(viewer);
 
 viewer.clock.shouldAnimate = true;
 Vue.prototype.viewer = viewer;
-var app = new Vue({
-  el: '#toolbar',
+const app = new Vue({
+  el: "#toolbar",
   data: {
     showSwitches: false,
     availableComponents: sats.availableComponents,
@@ -49,7 +49,7 @@ var app = new Vue({
       }
     }
   },
-})
+});
 
 sats.addFromTle("0 First-MOVE\n1 39439U 13066Z   18203.92296999 +.00000436 +00000-0 +59983-4 0  9994\n2 39439 097.5919 229.8528 0066721 040.9363 319.6832 14.81533022250876");
 sats.addFromTle("0 ISS (ZARYA)\n1 25544U 98067A   18243.71849541  .00002853  00000-0  50721-4 0  9994\n2 25544  51.6429   2.2915 0005795 114.7130 347.8859 15.53913300130267");
