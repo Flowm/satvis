32b71846511f51ffd128337d1df0065a26a539fb
Florian Mauracher <flo@mauracher.eu>
2018-09-22 20:33:41 +0200

    Only access timeline if it is enabled

--- src/modules/CesiumTimelineHelper.js
+++ src/modules/CesiumTimelineHelper.js
@@ -9,18 +9,28 @@ export class CesiumTimelineHelper {
     this.interval = undefined;
   }
 
+  get enabled() {
+    return (typeof this.viewer.timeline !== "undefined");
+  }
+
   clearInterval() {
     this.interval = undefined;
   }
 
   clearTimeline() {
     this.clearInterval();
+    if (!this.enabled) {
+      return;
+    }
     this.viewer.timeline._highlightRanges = [];
     this.viewer.timeline.updateFromClock();
     this.viewer.timeline.zoomTo(this.viewer.clock.startTime, this.viewer.clock.stopTime);
   }
 
   addHighlightRanges(ranges) {
+    if (!this.enabled) {
+      return;
+    }
     for (const range of ranges) {
       const startJulian = new Cesium.JulianDate.fromDate(new Date(range.start));
       const endJulian = new Cesium.JulianDate.fromDate(new Date(range.end));
