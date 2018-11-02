7cf6c9e4638fd0dab6f1b30944afd39bf28871c2
Florian Mauracher <flo@mauracher.eu>
2018-11-02 06:07:13 +0100

    ios: Create xcode project

---
+++ ios/satvis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  satvis
+//
+//  Created by Florian Mauracher on 02.11.18.
+//  Copyright © 2018 Florian Mauracher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
