fc4d36fce0283431b6c96568d26deba0cb6b7ca3
Florian Mauracher <flo@mauracher.eu>
2018-11-04 23:31:34 +0100

    ios: Add NotificationManager class

--- ios/satvis/AppDelegate.swift
+++ ios/satvis/AppDelegate.swift
@@ -3,9 +3,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var notificationManager: NotificationManager!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.notificationManager = NotificationManager.init()
         UIApplication.shared.statusBarView?.backgroundColor = UIColor.black
         return true
     }
@@ -30,6 +32,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    static func shared() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
     }
 }
 
---
+++ ios/satvis/lib/NotificationManager.swift
@@ -0,0 +1,47 @@
+import UIKit
+import UserNotifications
+
+class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
+    override init() {
+        super.init()
+        registerForPushNotifications()
+    }
+
+    func registerForPushNotifications(remote: Bool = false) {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            guard error == nil else {
+                NSLog("UNUserNotificationCenter ERR: \(String(describing: error))")
+                return
+            }
+            NSLog("UNUserNotificationCenter: Permission granted \(granted)")
+            guard granted else {
+                return
+            }
+            if (remote) {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
+
+    func triggerNotification(title: String, body: String, timeInterval: Double, categoryIdentifier: String, indentifier: String) {
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey: title, arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey: body, arguments: nil)
+        content.sound = UNNotificationSound.default
+        content.badge = NSNumber(integerLiteral: UIApplication.shared.applicationIconBadgeNumber + 1);
+        content.categoryIdentifier = categoryIdentifier
+        let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: timeInterval, repeats: false)
+        let request = UNNotificationRequest.init(identifier: indentifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+
+    func clearNotifications() {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+}
