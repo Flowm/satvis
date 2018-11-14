c6844e833144c52c7b053e5b56c3cd3f00ea484f
Florian Mauracher <flo@mauracher.eu>
2018-11-14 04:26:22 +0100

    ios: Display notifications while app is running

--- ios/satvis/lib/NotificationManager.swift
+++ ios/satvis/lib/NotificationManager.swift
@@ -39,6 +39,12 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
         UNUserNotificationCenter.current().add(request)
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+
     func clearNotifications() {
         UIApplication.shared.applicationIconBadgeNumber = 0
         UNUserNotificationCenter.current().removeAllDeliveredNotifications()
