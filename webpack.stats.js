bac991399acb77a40fc35e0e7d709a4ed573093e
Florian Mauracher <flo@mauracher.eu>
2018-12-12 03:15:38 +0100

    Add webpack bundle stats

---
+++ webpack.stats.js
@@ -0,0 +1,9 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.prod.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(common, {
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+});
