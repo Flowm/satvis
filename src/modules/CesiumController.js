7476c18adf647c58d17d083c021b51c9ad7bf9c2
Florian Mauracher <flo@mauracher.eu>
2018-12-13 03:49:54 +0100

    Reduce CPU utilization by limiting refresh rate when no entity is selected

--- src/modules/CesiumController.js
+++ src/modules/CesiumController.js
@@ -25,9 +25,11 @@ export class CesiumController {
     });
 
     // Cesium default settings
-    //this.viewer.scene.debugShowFramesPerSecond = true;
-    this.viewer.scene.globe.enableLighting = true;
     this.viewer.clock.shouldAnimate = true;
+    this.viewer.scene.globe.enableLighting = true;
+    this.viewer.scene.requestRenderMode = true;
+    this.viewer.scene.maximumRenderTimeChange = 1/30;
+    //this.viewer.scene.debugShowFramesPerSecond = true;
 
     // Export CesiumController for debugger
     window.cc = this;
