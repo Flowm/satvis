160fd0f9cb9af42fa2504ab3e351e863df2cff52
Florian Mauracher <flo@mauracher.eu>
2019-06-16 02:18:44 +0200

    ios: Add fastlane for screenshot automation

---
+++ ios/satvisUITests/satvisUITests.swift
@@ -0,0 +1,22 @@
+import XCTest
+
+class satvisUITests: XCTestCase {
+    override func setUp() {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("0Launch")
+    }
+}
