eb2d940e1b332ea03d3e3f583e3d2fc9fe0dd627
Florian Mauracher <flo@mauracher.eu>
2019-06-23 20:56:28 +0200

    Reorganize custom tle files

--- src/app.js
+++ src/app.js
@@ -14,16 +14,16 @@ const app = new Vue({
 window.app = app;
 
 /* global cc */
-//cc.sats.addFromTleUrl("data/tle/norad/active.txt", ["ACTIVE"]);
-cc.sats.addFromTleUrl("data/tle/norad/planet.txt", ["PLANET"]);
-cc.sats.addFromTleUrl("data/tle/norad/starlink.txt", ["STARLINK"]);
+//cc.sats.addFromTleUrl("data/tle/norad/active.txt", ["Active"]);
+cc.sats.addFromTleUrl("data/tle/norad/planet.txt", ["Planet"]);
+cc.sats.addFromTleUrl("data/tle/norad/starlink.txt", ["Starlink"]);
 cc.sats.addFromTleUrl("data/tle/norad/resource.txt", ["Resource"]);
 cc.sats.addFromTleUrl("data/tle/norad/science.txt", ["Science"]);
 cc.sats.addFromTleUrl("data/tle/norad/stations.txt", ["Stations"]);
 cc.sats.addFromTleUrl("data/tle/norad/weather.txt", ["Weather"]);
 
 cc.sats.addFromTleUrl("data/tle/custom/move.txt", ["MOVE"]);
-cc.sats.addFromTleUrl("data/tle/custom/ot.txt", ["OT"]);
+cc.sats.addFromTleUrl("data/tle/ext/ot.txt", ["OT"]);
 
 cc.sats.enableTag("MOVE");
 
