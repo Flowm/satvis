e913a1b860b20e27225d395cb64ace8043fea2d8
Florian Mauracher <flo@mauracher.eu>
2022-05-28 15:45:57 +0200

    Upgrade to oruga-next

--- src/components/Cesium.vue
+++ src/components/Cesium.vue
@@ -225,7 +225,7 @@
 
 <script>
 import Vue from "vue";
-import { Tooltip } from "@oruga-ui/oruga";
+import { Tooltip } from "@oruga-ui/oruga-next";
 import SatelliteSelect from "./SatelliteSelect.vue";
 import SatelliteMultiSelect from "./SatelliteMultiSelect.vue";
 import SatelliteNotifyMultiSelect from "./SatelliteNotifyMultiSelect.vue";
