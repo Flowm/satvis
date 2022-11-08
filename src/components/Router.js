4be4c6fe9ff3ec3e3917afa5d6aeabffe9023ab7
Florian Mauracher <flo@mauracher.eu>
2022-11-08 02:13:02 +0100

    Support deploying SPA to subfolders

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Cesium from "./Cesium.vue";
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(document.location.pathname.match(".*/")[0]),
   routes: [
     { path: "/", component: Cesium },
     { path: "/move.html", component: Cesium },
