9a27149f1ebcab13f1a23f34ff56abdfd99fe4ae
Florian Mauracher <flo@mauracher.eu>
2022-12-27 02:00:44 +0100

    Rename Satvis vue component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Cesium from "./Cesium.vue";
+
+import Satvis from "./Satvis.vue";
 
 export default createRouter({
   history: createWebHistory(document.location.pathname.match(".*/")[0]),
   routes: [
-    { path: "/", component: Cesium },
-    { path: "/move.html", component: Cesium },
-    { path: "/ot.html", component: Cesium },
+    { path: "/", component: Satvis },
+    { path: "/move.html", component: Satvis },
+    { path: "/ot.html", component: Satvis },
   ],
 });
