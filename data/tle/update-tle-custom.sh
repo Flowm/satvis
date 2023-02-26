a623eb9ed3211208b6793c2cd6aef524c838ec7a
Florian Mauracher <flo@mauracher.eu>
2023-02-26 03:01:01 +0100

    Update custom tle import

--- data/tle/update-tle-custom.sh
+++ data/tle/update-tle-custom.sh
@@ -8,6 +8,7 @@ node update-tle.js
 # Additional TLE groups for the satvis.space deployment
 egrep -A2 '(FIRST-MOVE|MOVE-II)' --no-group-separator groups/active.txt > custom/move.txt
 egrep -A2 '(LEMUR-2-ROHOVITHSA)' --no-group-separator groups/active.txt > custom/ot.txt
+sed -i "s/LEMUR-2-ROHOVITHSA/FOREST-1/g" custom/ot.txt
 cat ext/ot-add.txt >> custom/ot.txt 2> /dev/null
-egrep -A2 '^(SENTINEL-2A|SENTINEL-2B|SENTINEL-3A|SENTINEL-3B|AQUA|TERRA |SUOMI NPP|NOAA 20|NOAA 21|METEOSAT-8|METEOSAT-10|METEOSAT-11|GOES 16|GOES 17|GOES 18|HIMAWARI-8|METOP-A|METOP-B|METOP-C|LANDSAT 8|LANDSAT 9|FENGYUN 3D|GEO-KOMPSAT-2A)' --no-group-separator groups/active.txt > custom/wfs.txt
+egrep -A2 '^(SENTINEL-2A|SENTINEL-2B|SENTINEL-3A|SENTINEL-3B|AQUA|TERRA |SUOMI NPP|NOAA 20|NOAA 21|METEOSAT-8|METEOSAT-10|METEOSAT-11|GOES 16|GOES 17|GOES 18|HIMAWARI-8|HIMAWARI-9|METOP-A|METOP-B|METOP-C|LANDSAT 8|LANDSAT 9|FENGYUN 3D|GEO-KOMPSAT-2A|GCOM-C)' --no-group-separator groups/active.txt > custom/wfs.txt
 cat ext/wfs-add.txt >> custom/wfs.txt 2> /dev/null
